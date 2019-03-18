30cbe56f3b2be603386e8f218193a8db13a7071c
Richard <Richard>
2019-03-18 23:51:40 +0000

    adding to string

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub fn cstr(s: &str) -> CString {
     std::ffi::CString::new(s).unwrap().into_raw() as i32
 }
 
-pub fn cstr_to_owned(c: CString) -> String {
+pub fn cstr_to_string(c: CString) -> String {
     let s: &CStr = unsafe { CStr::from_ptr(c as *const i8) };
     s.to_str().unwrap().to_owned()
 }
