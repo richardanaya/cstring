be647f300b2ebe64113d5c06d670ae4925f83a97
Richard <Richard>
2019-03-18 23:49:12 +0000

    adding capability to get strings from cstring

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
-pub fn cstr(s: &str) -> i32 {
+use std::ffi::CStr;
+
+pub type CString = i32;
+
+pub fn cstr(s: &str) -> CString {
     std::ffi::CString::new(s).unwrap().into_raw() as i32
+}
+
+pub fn cstr_to_owned(c: CString) -> String {
+    let s: &CStr = unsafe { CStr::from_ptr(c as *const i8) };
+    s.to_str().unwrap().to_owned()
 }
