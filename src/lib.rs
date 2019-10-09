e40727ab9d1efd5bd9405b46f29d0f1c0ef4e192
richard <richard>
2019-10-09 14:12:22 -0700

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
-use std::ffi::CStr;
+#![no_std]
+extern crate alloc;
+use cstr_core::*;
+use crate::alloc::borrow::ToOwned;
+use alloc::string::String;
 
 pub type CString = i32;
 
 pub fn cstr(s: &str) -> CString {
-    std::ffi::CString::new(s).unwrap().into_raw() as i32
+    cstr_core::CString::new(s).unwrap().into_raw() as i32
 }
 
 pub fn cstr_to_string(c: CString) -> String {
