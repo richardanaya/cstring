6c8d22d8a8db8e698bea9b804eea896659c73a90
Richard <Richard>
2020-11-22 08:56:09 -0800

    new api

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,24 @@ mod cstr_core;
 extern crate alloc;
 use crate::alloc::borrow::ToOwned;
 use alloc::string::String;
+use core::convert::TryInto;
 
 pub type CString = i32;
 
-pub fn cstr(s: &str) -> CString {
+pub fn from_str(s: &str) -> CString {
     cstr_core::CString::new(s).unwrap().into_raw() as i32
 }
 
-pub fn cstr_to_string(c: impl Into<CString>) -> String {
-    let s: &cstr_core::CStr =
-        unsafe { cstr_core::CStr::from_ptr(c.into() as *const cstr_core::c_char) };
-    s.to_str().unwrap().to_owned()
+pub fn try_into_string(start: impl TryInto<CString>) -> Result<String, &'static str> {
+    if let Ok(pos) = start.try_into() {
+        let s: &cstr_core::CStr =
+            unsafe { cstr_core::CStr::from_ptr(pos as *const cstr_core::c_char) };
+        if let Ok(s) = s.to_str() {
+            Ok(s.to_owned())
+        } else {
+            Err("error creating cstring")
+        }
+    } else {
+        Err("could not decypher cstring starting point")
+    }
 }
